93b54975b8b685cc7b0574ef0d3bda8b8a8717c1
TobiNguyen <nat21102002@gmail.com>
2023-08-13 22:55:33 +0700

    func9: fix expires - save token to cookie

--- app/configs/system.js
+++ app/configs/system.js
@@ -2,5 +2,6 @@
 module.exports = {
     prefixAdmin: 'adminCCC',
     JWT_SECRET: '123',
-    JWT_EXP: '1h'
+    JWT_EXP: '1h',
+    COOKIE_EXP: 30
 };
--- app/utils/saveTokenToCookie.js
+++ app/utils/saveTokenToCookie.js
@@ -2,15 +2,17 @@ const systemConfig = require('../configs/system');
 
 const saveTokenToCookie = (res, statusCode, token) => {
     const options = {
-        exprires: new Date(Date.now() + systemConfig.JWT_EXP * 24 * 60 * 60 * 1000),
+        expires: new Date(Date.now() + systemConfig.COOKIE_EXP * 24 * 60 * 60 * 1000),
         httpOnly: true
-    }
+    };
+
+    console.log("exp: ", options);
 
     res.status(statusCode).cookie('token', token, options)
         .json({
             success: true,
             token
-        })
+        });
 }
 
 module.exports = saveTokenToCookie;
